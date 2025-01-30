80c58092d3f8f2751f338abb58e656d4d0ab7863
Nathan V. Patwardhan <npatwardhan@netboxlabs.com>
2025-01-30 13:54:00 -0500

    Moves functions to AnsibleAutomationAWXManager methods

--- setup/configure_ansible_automation.py
+++ setup/configure_ansible_automation.py
@@ -5,7 +5,7 @@ import argparse
 import time
 import yaml
 
-from helpers.ansible_automation import AnsibleAutomation
+from helpers.ansible_automation_awx_manager import AnsibleAutomationAWXManager
 
 
 def get_arguments():
@@ -28,203 +28,6 @@ def get_arguments():
 
     # Return the parsed arguments
     return args
-
-
-def create_aa_organization(aa_obj = None, org_name = None):
-    org_payload = {
-        'name': org_name
-    }
-
-    return aa_obj.create_object('organizations', org_name, org_payload)
-
-
-def create_aa_inventory(aa_obj = None, inventory_name = None, org_id = 0):
-    inventory_payload = {
-        'name': inventory_name,
-        'organization': org_id
-    }
-
-    return aa_obj.create_object('inventory', inventory_name, inventory_payload)
-
-
-def create_aa_host(aa_obj = None, inventory_id = 0, host_name = None, is_enabled = False, var_data = None):
-    host_payload = {
-        'name': host_name,
-        'enabled': is_enabled,
-        'inventory': inventory_id,
-        'variables': var_data
-    }
-
-    return aa_obj.create_object('hosts', host_name, host_payload)
-
-
-def create_aa_execution_environment(aa_obj = None, ee_name = None, ee_image_name = None, ee_reg_cred_id = 0, org_id = 0):
-    ee_payload = {
-        'name': ee_name,
-        'image': ee_image_name,
-        'organization': org_id
-    }
-
-    if ee_reg_cred_id:
-        ee_payload['credential'] = ee_reg_cred_id
-
-    return aa_obj.create_object('execution_environments', ee_name, ee_payload)
-
-
-def create_aa_project(aa_obj = None, project_name = None, scm_type = None, scm_url = None, scm_branch = None, org_id = 0, ee_id = 0):
-    project_payload = {
-        'name': project_name,
-        'organization': org_id,
-        'scm_type': scm_type,
-        'scm_url': scm_url,
-        'scm_branch': scm_branch,
-        'default_environment': ee_id
-    }
-
-    created_project = aa_obj.create_object('projects', project_name, project_payload)
-
-    # Wait for project completion to complete, pausing 0.25s per iteration
-    while True:
-        proj_status = aa_obj.get_object_by_name('projects', project_name)
-
-        if proj_status['status'] == 'successful':
-            break
-
-        if proj_status['status'] == 'failed':
-            print("failed to sync project playbooks")
-            sys.exit(0)
-
-        time.sleep(0.25)
-
-    return created_project
-
-
-def create_aa_credential_type(aa_obj = None, credential_type_name = None):
-    ct_payload = {
-        'name': credential_type_name,
-        'kind': "cloud",
-        'inputs': {
-            'fields': [
-                {'id': "proxmox_api_host", 'type': "string", 'label': "Proxmox API Host"},
-                {'id': "proxmox_api_user", 'type': "string", 'label': "Proxmox API User"},
-                {'id': "proxmox_api_user_token", 'type': "string", 'label': "Proxmox API Token ID"},
-                {'id': "proxmox_node", 'type': "string", 'label': "Proxmox Node"},
-                {'id': "proxmox_api_token_secret", 'type': "string", 'label': "Proxmox API Token", 'secret': True},
-                {'id': "netbox_api_proto", 'type': "string", 'label': "NetBox HTTP Protocol"},
-                {'id': "netbox_api_host", 'type': "string", 'label': "NetBox API host"},
-                {'id': "netbox_api_port", 'type': "string", 'label': "NetBox API port"},
-                {'id': "netbox_api_token", 'type': "string", 'label': "NetBox API token", 'secret': True}
-            ],
-            'required': ['proxmox_api_host',
-                         'proxmox_api_user',
-                         'proxmox_api_user_token',
-                         'proxmox_node',
-                         'proxmox_api_token_secret',
-                         'netbox_api_host',
-                         'netbox_api_port',
-                         'netbox_api_proto',
-                         'netbox_api_token'
-            ]
-        },
-        'injectors': {
-            'extra_vars': {
-                "netbox_env_info": {
-                    "api_host": '{{ netbox_api_host }}',
-                    "api_port": '{{ netbox_api_port }}',
-                    "api_proto": '{{ netbox_api_proto }}',
-                    "api_token": '{{ netbox_api_token }}'
-                },
-                "proxmox_env_info": {
-                    "node": '{{ proxmox_node }}',
-                    "api_host": '{{ proxmox_api_host }}',
-                    "api_user": '{{ proxmox_api_user }}',
-                    "api_token_id": '{{ proxmox_api_user_token }}',
-                    "api_token_secret": '{{ proxmox_api_token_secret }}'
-                }
-            }
-        }
-    }
-
-    return aa_obj.create_object('credential_types', credential_type_name, ct_payload)
-
-
-def create_aa_credential(aa_obj = None, credential_name = None, credential_type_id = 0, org_id = 0, netbox_api_config = {}, proxmox_api_config = {}):
-    credential_payload = {
-        'name': credential_name,
-        'credential_type': credential_type_id,
-        'organization': org_id,
-        'inputs': {
-            'netbox_api_host': netbox_api_config['api_host'],
-            'netbox_api_port': str(netbox_api_config['api_port']),
-            'netbox_api_proto': netbox_api_config['api_proto'],
-            'netbox_api_token': netbox_api_config['api_token'],
-            'proxmox_node': proxmox_api_config['node'],
-            'proxmox_api_host': proxmox_api_config['api_host'],
-            'proxmox_api_user': proxmox_api_config['api_user'],
-            'proxmox_api_user_token': proxmox_api_config['api_token_id'],
-            'proxmox_api_token_secret': proxmox_api_config['api_token_secret']
-        }
-    }
-
-    return aa_obj.create_object('credentials', credential_name, credential_payload)
-
-
-def get_aa_playbooks(aa_obj = None, project_id = 0):
-    aa_playbooks = []
-
-    for playbook_item in aa_obj.get_object_by_id('projects', project_id).get_related('playbooks'):
-        if playbook_item.endswith('.yml') or playbook_item.endswith('.yaml'):
-            if not re.search(r'/', playbook_item):
-                aa_playbooks.append(playbook_item)
-
-    return aa_playbooks
-
-
-def create_aa_job_template(aa_obj = None, playbook_name = None, inventory_id = 0, org_id = 0, ee_id = 0, project_id = 0):
-    job_template_name = re.sub(r'^awx\-', 'XX', playbook_name.split('.')[0])
-
-    job_template_payload = {
-        'name': job_template_name,
-        'job_type': 'run',
-        'inventory': inventory_id,
-        'organization': org_id,
-        'project': project_id,
-        'execution_environment': ee_id,
-        'playbook': playbook_name,
-        'ask_variables_on_launch': True,
-        'ask_credential_on_launch': True
-    }
-
-    return aa_obj.create_object('job_templates', job_template_name, job_template_payload)
-    
-
-def create_aa_job_template_credential(aa_obj = None, template_id = 0, cred_name = None):
-    the_cred = aa_obj.get_object_by_name('credentials', cred_name)
-
-    if not the_cred:
-        raise ValueError(f"Unable to get credentials object for {cred_name}")
-        
-    jt_entry = aa_obj.get_object_by_id('job_templates', template_id)
-
-    if not jt_entry:
-        raise ValueError(f"Unable to find job_templates {template_id}")
-
-    if not jt_entry['summary_fields']['credentials']:
-        try:
-            jt_entry.add_credential(the_cred)
-        except:
-            return False
-        
-    return True
-
-
-def get_aa_project_related_templates(aa_obj = None, project_id = 0):
-    related_templates = []
-
-    for item in aa_obj.get_objects_by_kwargs('job_templates', project=project_id):
-        related_templates.append(aa_obj.get_object_by_id('job_templates', item.id))
-
-    return related_templates
 
 
 def main():
@@ -314,124 +117,77 @@ def main():
     # End set common variables
 
     
-    aa = AnsibleAutomation(app_config['ansible_automation'])
+    aam = AnsibleAutomationAWXManager(app_config)
 
     if args.action_type == 'create':
-        created_organization = create_aa_organization(aa, org_name)
+        aam.create_organization(org_name)
+        print(aam.org_id)
 
-        if not created_organization:
-            print(f"Unable to create organization {org_name}")
-            sys.exit(1)
+        aam.create_inventory(inventory_name)
+        print(aam.inventory_id)
 
-        org_id = created_organization['id']
-        created_inventory = create_aa_inventory(aa, inventory_name, org_id)
+        aam.create_host(host_name, host_var_data)
+        print(aam.host_id)
 
-        if not created_inventory:
-            print(f"Unable to create inventory {inventory_name}")
-            sys.exit(1)
+        aam.create_execution_environment(ee_name, ee_image_name)
+        print(aam.ee_id)
 
-        inventory_id = created_inventory['id']
-        created_host = create_aa_host(aa, inventory_id, host_name, True, host_var_data)
+        aam.create_project(project_name, scm_type, scm_url, scm_branch)
+        print(aam.project_id, aam.project)
 
-        if not created_host:
-            print(f"Unable to create hosts {host_name}")
-            sys.exit(1)
-
-        created_host_id = created_host['id']
-
-        created_ee = create_aa_execution_environment(aa, ee_name, ee_image_name, 0, org_id)
-        created_ee_id = created_ee['id']
-
-        if not created_ee:
-            print(f"Unable to create execution environment {ee_name}")
-            sys.exit(1)
-
-        created_project = create_aa_project(aa, project_name, scm_type, scm_url, scm_branch, org_id, created_ee_id)
-
-        if not created_project:
-            print(f"Unable to create project {project_name}")
-            sys.exit(1)
-
-        project_id = created_project['id']
-
-        project_playbooks = get_aa_playbooks(aa, project_id)
+        project_playbooks = aam.get_playbooks()
+        project_playbooks = [x for x in project_playbooks if x.startswith('awx-')]
+        #print("project playbooks", project_playbooks)
 
         if not project_playbooks:
-            print(f"Unable to collect project playbooks for {project_name}")
+            print("I can't find any project playbooks.  Therefore I cannot create any templates.  Exiting.")
             sys.exit(1)
 
-        project_playbooks = [x for x in project_playbooks if x.startswith('awx-')]
-        #print("PP", project_playbooks)
+        aam.create_credential_type(credential_type)
+        print(aam.credential_type_id)
 
-        create_credential_type = create_aa_credential_type(aa, credential_type)
+        aam.create_credential(credential_name)
+        print(aam.credential_id)
 
-        if not create_credential_type:
-            print(f"Unable to create credential type {credential_type}")
-            sys.exit(1)
-
-        create_credential_type_id = create_credential_type['id']
-
-        create_credential = create_aa_credential(aa, credential_name, create_credential_type_id, org_id, app_config['netbox_api_config'], app_config['proxmox_api_config'])
-
-        if not create_credential:
-            print(f"Unable to create credential {credential_name}")
-            sys.exit(1)
-
-        created_job_templates = []
         for project_playbook in project_playbooks:
-            created_job_template = create_aa_job_template(aa, project_playbook, inventory_id, org_id, created_ee_id, project_id)
-            created_job_templates.append(created_job_template)
+            aam.create_job_template(project_playbook)
+        print("CJT", aam.created_job_templates)
 
-        for created_job_template_item in created_job_templates:
-            create_aa_job_template_credential(aa, created_job_template_item['id'], credential_name)
-
-        #print(org_id, inventory_id, project_id, created_project['related']['playbooks'], create_credential_id, create_credential_type_id)
+        for created_job_template_item in aam.created_job_templates:
+            aam.create_job_template_credential(created_job_template_item['id'])
     elif args.action_type == 'destroy':
-        project_name = default_project
-        if 'project' in app_config['ansible_automation']['settings']:
-            if 'name' in app_config['ansible_automation']['settings']['project']:
-                project_name = app_config['ansible_automation']['settings']['project']['name']
+        aam.get_project(project_name)
 
-        get_project = aa.get_object_by_name('projects', project_name)
+        aam.get_job_templates_for_project()
 
-        if not get_project:
-            print(f"Unable to get project {project_name}")
+        if not aam.job_templates:
+            print(f"Unable to find any job templates for project {project_name}!")
             sys.exit(1)
 
         collected_credentials = {}
-        related_project_templates = get_aa_project_related_templates(aa, get_project['id'])
-        for related_project_template in related_project_templates:
-            related_credentials = aa.get_object_by_id('credentials', related_project_template.get_related('credentials').results[0]['id'])
+        for project_template in aam.job_templates:
+            related_credentials = aam.get_object_by_id('credentials', project_template.get_related('credentials').results[0]['id'])
 
             if related_credentials:
                 related_credentials_type = aa.get_object_by_id('credential_types', related_credentials.get_related('credential_type')['id'])
                 collected_credentials[related_credentials['name']] = related_credentials_type['name']
 
-            aa.delete_object(related_project_template)
+            aam.delete_job_template(project_template)
 
-        delete_credential = False
+        aam.delete_project()
+
         if credential_name in collected_credentials:
-            delete_credential = True
+            aam.delete_credential(credential_name)
 
-        delete_credential_type = False
-        if delete_credential:
-            if collected_credentials[credential_name] == credential_type:
-                delete_credential_type = True
-
-        if delete_credential:
-            aa.delete_object_by_name('credentials', credential_name)
-
-        if delete_credential_type:
-            aa.delete_object_by_name('credential_types', credential_type)
-
-        aa.delete_object(get_project)
+        if collected_credentials[credential_name] == credential_type:
+            aam.delete_credential_type(credential_type)
 
         if host_name != default_host_name:
-            aa.delete_object_by_name('hosts', host_name)
+            aam.delete_host(host_name)
 
         if inventory_name != default_inventory:
-            aa.delete_object_by_name('inventory', inventory_name)
-            
+            aam.delete_inventory(inventory_name)
+
     sys.exit(0)
 
 
